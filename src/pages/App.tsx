63ed368f784b74b21bf9d7edfa9f11d12e09348c
Brown Mr <pavel.b18f@gmail.com>
2023-03-23 00:52:42 +0200

    Added main body component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import Pokemons from "./Pokemons/Pokemons";
 
 function App() {
   return (
     <>
-      <header></header>
-      <main></main>
-      <footer></footer>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Pokemons />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
---
+++ src/pages/Pokemons/Pokemons.tsx
@@ -0,0 +1,10 @@
+import { Container, Stack } from "react-bootstrap";
+import "./Pokemons.module.css";
+
+export default function Pokemons() {
+  return (
+    <Container className="wrapper">
+      <Stack gap={4}>test</Stack>
+    </Container>
+  );
+}
