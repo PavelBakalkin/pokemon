c4019b5a69eeec2237e52af536573abcc397150a
Brown Mr <pavel.b18f@gmail.com>
2023-03-23 00:16:04 +0200

    Prepared files

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
